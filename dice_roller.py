bc36c9999b6d98f9b65370f36c727e65d5879385
karthigeyan ganesh shankar <git@github.com:karthigeyanrgs/Seminar.git>
2020-11-23 22:02:37 +0530

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,18 @@
 import random
 def main():
-	dice_rolls = 2
+	dice_rolls = int(input("How many inputs"))
 	dice_sum = 0
+	dice_size = int(input("How many sides of dice"))
 	for i in range(0,dice_rolls):
 		roll = random.randint(1,6)
 		dice_sum += roll
-		print('You rolled a %d' % roll)
-		print('Roll sum %d' % dice_sum)
+		if roll == 1:
+			print('You rolled a critical fail')
+		elif roll == dice_size:
+			print('You rolled a critical success')
+		else:
+			print('You rolled a %d' % roll)
+			print('Roll sum %d' % dice_sum)
 
 if __name__== "__main__":
 	main()
